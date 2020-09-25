81cc37812bf268b5918d4f0c9a299905c0ce275c
Saac Sornchai <sornchai.la@ku.th>
2020-09-25 12:04:34 +0700

    add interface StudentDataSource

--- src/main/java/ku/cs/classroom/services/StudentDataSource.java
+++ src/main/java/ku/cs/classroom/services/StudentDataSource.java
@@ -1,2 +1,9 @@
-package ku.cs.classroom.services;public interface StudentDataSource {
+package ku.cs.classroom.services;
+
+import ku.cs.classroom.models.StudentList;
+
+public interface StudentDataSource {
+    StudentList getStudentsData();
+
+    void setStudentsData(StudentList students);
 }
